6c52004f92894a2dfbf1a948baf298e513fa74b8
Изотов Данил <rusbeks@outlook.com>
2022-05-29 10:03:28 +1000

    initial commit

---
+++ CanvasDrawer.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Match_3
+{
+    public class CanvasDrawer
+    {
+        public CanvasDrawer(Canvas canvas)
+        {
+            Target = canvas;
+        }
+
+
+        private readonly Canvas Target;
+
+
+        public void AddUIElementToCanvas(UIElement element)
+        {
+            Target.Children.Add(element);
+        }
+
+        public void RemoveUIElementToCanvas(UIElement element)
+        {
+            Target.Children.Remove(element);
+        }
+
+        public void SetUIElementPosition(UIElement element, int x, int y)
+        {
+            Canvas.SetTop(element, y);
+            Canvas.SetLeft(element, x);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace Match_3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Drawer = new CanvasDrawer(GameCanvas);
+            Timer = new DispatcherTimer();
+        }
+
+
+        private readonly CanvasDrawer Drawer;
+        private readonly DispatcherTimer Timer;
+
+
+        private void OnTimerTick(object sender, System.EventArgs e)
+        {
+        }
+
+        private void OnMainWindowLoaded(object sender, System.EventArgs e)
+        {
+            Timer.Tick += OnTimerTick;
+        }
+    }
+}
+
+namespace Match_3.Core
+{
+    public class GameCore
+    {
+        public float Time { get; private set; }
+        public float DeltaTime { get; private set; }
+
+
+        public void Tick(float time)
+        {
+            DeltaTime = time - Time;
+            Time = time;
+
+
+        }
+    }
+
+    public class Grid
+    {
+    }
+
+    public class Cell
+    {
+    }
+
+    public class Gem
+    {
+    }
+}
